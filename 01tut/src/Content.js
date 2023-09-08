926810e3551936fe956b2813fcfda3c5edc4ca2e
Dani Mardiana <danimardiana18@gmail.com>
2023-09-08 09:08:42 +0000

    Lesson 7-8

--- 01tut/src/Content.js
+++ 01tut/src/Content.js
@@ -1,34 +1,21 @@
 import React from 'react'
-import { useState } from 'react';
+import ItemList from './ItemList'
 
-const Content = () => {
-    const [name, setName] = useState('Dave')
-    const [count, setCount] = useState(0)
-
-    const handleNameChange = () => {
-        const names = ['Bob','Kevin','Dave'];
-        const int = Math.floor(Math.random() * 3);
-        setName(names[int]);
-    }
-
-    const handleClick = () => {
-        setCount(count+1)
-        setCount(count+1)
-        console.log(count);
-    }
-    
-    const handleClick2 = () => {
-        console.log(count)
-    }
+const Content = ({ items, handleCheck, handleDelete}) => {
     
     return (
         <main>
-            <p onDoubleClick={handleClick}>
-                Hello {name}!
-            </p>
-            <button onClick={handleNameChange}>Change Name</button>
-            <button onClick={handleClick}>Click It</button>
-            <button onClick={handleClick2}>Click It</button>
+            {items.length ? (
+                <ItemList 
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{
+                    marginTop: '2rem'
+                }}>Your list is empty</p>
+            )}
         </main>
     )
 }
