3069bde83c067bc0e610755ff49d365bd2de2f70
Dani Mardiana <danimardiana18@gmail.com>
2023-10-27 06:44:03 +0000

    14 tut

---
+++ 14tut/src/Content.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ItemList from './ItemList'
+
+const Content = ({ items, handleCheck, handleDelete}) => {
+    
+    return (
+        <>
+            {items && items.length  ? (
+                <ItemList 
+                    items={items}
+                    handleCheck={handleCheck}
+                    handleDelete={handleDelete}
+                />
+            ) : (
+                <p style={{
+                    marginTop: '2rem'
+                }}>Your list is empty</p>
+            )}
+        </>
+    )
+}
+
+export default Content
